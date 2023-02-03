be854e97b03f42468c5df311e311f748c4406358
Infosec Eagle <alburnett@gmail.com>
2023-02-02 21:47:12 -0500

    Create do-b64.py

---
+++ do-b64.py
@@ -0,0 +1,17 @@
+#*******************************************************************
+#* Author = Adam Burnett (@infosecEagle)                           *
+#* simple quick script for turning human readable text into Base64 *
+#*                                                                 *
+#*                                                                 *
+#*                                                                 *
+#*******************************************************************
+
+import base64
+
+plain = input("drop plain text here ")
+plain_string = plain.encode("ascii")
+
+base64_bytes = base64.b64encode(plain_string)
+base64_string = base64_bytes.decode("ascii")
+
+print(base64_string)
