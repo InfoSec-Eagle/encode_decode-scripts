19ee96e8f1e775fb063599260c29742afe517db7
Infosec Eagle <alburnett@gmail.com>
2023-02-02 21:46:35 -0500

    Create de-hex.py

---
+++ de-hex.py
@@ -0,0 +1,16 @@
+#*******************************************************************
+#* Author = Adam Burnett (@infosecEagle)                           *
+#* simple quick script for turning hex into human readable text    *
+#*                                                                 *
+#*                                                                 *
+#* use python3 ./de-hex.py                                         *
+#*******************************************************************
+
+hex_Input = input("drop Hex here ")
+print(bytes.fromhex(hex_Input).decode('utf-8'))
+
+#*******************************************************************
+# To-Do:                                                           *
+# 1) Chose decoding; ie: ASCII, UTF,... Maybe both?                *
+#                                                                  *
+#*******************************************************************
